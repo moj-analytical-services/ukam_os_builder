d5d78b5584e928e4f667fc08b45a92d860e0a320
ThomasHepworth <thomas.hepworth@justice.gov.uk>
2026-08-19 16:03:28 +0100

    build: bump to `v0.3.1`

--- ukam_os_builder/__init__.py
+++ ukam_os_builder/__init__.py
@@ -8,7 +8,7 @@ from ukam_os_builder.os_builder.inspect_results import (
     inspect_flatfile_variants,
 )
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 __all__ = [
     "create_config_and_env",
